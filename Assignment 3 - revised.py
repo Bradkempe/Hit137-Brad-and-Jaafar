2a12ce7a009aaee3fbc3a6ef50edae2827eba0ea
jafar-hit <jaafar.mehydeen@students.cdu.edu.au>
2025-02-02 08:05:58 +1100

    Add files via upload

---
+++ Assignment 3 - revised.py
@@ -0,0 +1,7 @@
+print("Code Start")
+print("Hello my name is","\"Sarah\"")
+print("I'm learning Python.")
+print("End Code")
+price = 50 # Initial price
+price = price * (1 - 0.15) # Applying the discount
+print(price)
