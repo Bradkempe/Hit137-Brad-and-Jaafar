56ed7724ba42c55131307f8dedef9cd9e57b1af5
jafar-hit <jaafar.mehydeen@students.cdu.edu.au>
2025-02-02 08:17:23 +1100

    Delete Assignment 3 - revised.py

--- Assignment 3 - revised.py
+++
@@ -1,7 +0,0 @@
-print("Code Start")
-print("Hello my name is","\"jafar\"")
-print("I'm learning Python.")
-print("End Code")
-price = 50 # Initial price
-price = price * (1 - 0.15) # Applying the discount
-print(price)
